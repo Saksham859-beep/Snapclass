81be25eead0b46fc355483ef32b8fad059011a48
Saksham859-beep <jainsaksham773@gmail.com>
2026-06-29 13:46:58 +0530

    Refactor unenroll button and update placeholder text

--- ai-attendance-project-app-main/ai-attendance-project-app-main/src/screens/student_screen.py
+++ ai-attendance-project-app-main/ai-attendance-project-app-main/src/screens/student_screen.py
@@ -61,36 +61,29 @@ def student_dashboard():
 
     cols = st.columns(2)
     for i, sub_node in enumerate(subjects):
-    sub = sub_node["subjects"]
-    sid = sub["subject_id"]
+        sub = sub_node['subjects']
+        sid = sub['subject_id']
 
-    stats = stats_map.get(sid, {"total": 0, "attended": 0})
 
-    def unenroll_button(sid=sid, subject_name=sub["name"]):
-        if st.button(
-            "Unenroll from this course",
-            key=f"unenroll_{sid}",
-            type="tertiary",
-            width="stretch",
-            icon=":material/delete_forever:"
-        ):
-            unenroll_student_to_subject(student_id, sid)
-            st.toast(f"Unenrolled from {subject_name} successfully!")
-            st.rerun()
+        stats = stats_map.get(sid,{"total":0, "attended": 0} )
+        def unenroll_button():
+                if st.button("Unenroll from tihs course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
+                    unenroll_student_to_subject(student_id, sid)
+                    st.toast(f'Unenrolled from {sub['name']} successfully!')
+                    st.rerun()
 
-    with cols[i % 2]:
-        subject_card(
-            name=sub["name"],
-            code=sub["subject_code"],
-            section=sub["section"],
-            stats=[
-                ("📅", "Total", stats["total"]),
-                ("✅", "Attended", stats["attended"]),
-            ],
-            footer_callback=unenroll_button,
-        )
+        with cols[i % 2]:
 
-        
+            subject_card(
+                name = sub['name'],
+                code =sub['subject_code'],
+                section = sub['section'],
+                stats = [
+                    ('📅', 'Total', stats['total']),
+                    ('✅', 'Attended', stats['attended']),
+                ],
+                footer_callback=unenroll_button
+            )
     footer_dashboard()
 
 
@@ -149,7 +142,7 @@ def student_screen():
     if show_registration:
         with st.container(border=True):
             st.header('Register new Profile')
-            new_name = st.text_input("Enter your name", placeholder='E.g. Saksham Jain')
+            new_name = st.text_input("Enter your name", placeholder='E.g. Hamza Rizvi')
 
             st.subheader('Optional : Voice Enrollment')
             st.info("Enroll your for voice only attendance")
